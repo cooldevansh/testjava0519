139f7c8cd2174666636f6bad36a9434cad9fce15
dec led <pkaipa@gmail.com>
2026-05-25 17:47:33 -0400

    commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main{
+    public static void main(String[]  args){
+        car Car1= new car("Honda", "crv");
+        car Car2= new car("Tayota", "carola");
+        For(i<car.count); i++){
+        System.out.println(Car+i.Make);
+        System.out.println(Car1.Model);
+        System.out.println(Car1.IsCarOn);
+
+
+    }
+
+}
---
+++ src/car.java
@@ -0,0 +1,12 @@
+public class car{
+    String Make;
+    String Model;
+    Boolean IsCarOn;
+
+
+    car(String make, String model){
+     this.Make = make;
+     this.Model = model;
+     this.IsCarOn = false ;
+      }
+    }
